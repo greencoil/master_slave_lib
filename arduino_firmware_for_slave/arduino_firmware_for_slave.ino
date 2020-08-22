66bb15cfc054d40c8d2909ebb1c26342f3ccf61d
yamaguchi <yamaguchi@miuuchi.lab.tuat.ac.jp>
2020-08-23 00:32:58 +0900

    check echo function with dynamixelSDK

--- arduino_firmware_for_slave/arduino_firmware_for_slave.ino
+++ arduino_firmware_for_slave/arduino_firmware_for_slave.ino
@@ -8,7 +8,8 @@ void print_input_data(char data_buf){
 }
 
 void setup(){
-  Serial.begin(115200);
+  Serial.begin(57600);
+  Serial1.begin(115200);
   while (!Serial);
 }
 
@@ -26,19 +27,18 @@ bool check_targetID(char data_buf[]){
 void data_extraction(char data_buf[]){
   ;
 }
-void buf_flush(){
+void buf_flush(char data_buf[]){
   ;
 }
 
 void loop() {
-  
   //データ格納部分
   if(Serial.available()){
     data_buf[buf_counter]=Serial.read();
     buf_counter++;
     if(buf_counter>BUF_SIZE-1){
       buf_counter=0;
-    } 
+    }
     Serial.write(data_buf,BUF_SIZE);
     // Serial.println();
   }
@@ -47,10 +47,14 @@ void loop() {
     if(check_CRC){
       if(check_targetID(data_buf)){
         data_extraction(data_buf);
-        Buf_flush(data_buf);
+        buf_flush(data_buf);
       }
     }
   }
-//  Serial.println(data_buf);
-  delay(1);
+  for(int i=0;i<buf_counter;i++){
+    byte buf_tmp=data_buf[i];
+    Serial1.print(buf_tmp,HEX);
+  }
+    delay(1);
+    Serial1.println();
 }
