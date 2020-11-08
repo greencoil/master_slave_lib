12a26b14342fe7e84821870831991e1ecd3dbac9
yamaguchi <yamaguchi@miuuchi.lab.tuat.ac.jp>
2020-11-09 01:10:55 +0900

    length解釈まで

--- arduino_firmware_for_slave/arduino_firmware_for_slave.ino
+++ arduino_firmware_for_slave/arduino_firmware_for_slave.ino
@@ -1,74 +1,174 @@
-#define BUF_SIZE 50
+#define BUFF_SIZE 40
 #define END 0
 #define ID 1
-char data_buf[BUF_SIZE]={};
-int8_t buf_counter=0;
-
-void print_input_data(char data_buf){
+byte HEADER[]={0xFF,0xFF,0xFD,0x00};
+byte data_buff[BUFF_SIZE] = {};
+uint8_t buff_head = 0;
+uint8_t data_head = 0;
+bool wait_receive_complete = 0;
+bool check_targetID_flag = 0 ;
+bool check_data_length_flag = 0;
+uint8_t index_ID = 0;
+uint8_t index_LENGTH_H = 0;
+uint8_t index_LENGTH_L = 0;
+uint8_t index_INST = 0;
+void print_input_data(byte data_buff) {
   ;
 }
 
-void setup(){
-  Serial.begin(57600);
-  Serial1.begin(115200);
+void setup() {
+  Serial.begin(115200);//PC
+  Serial1.begin(115200);//SERIAL
   while (!Serial);
+  Serial1.println("--------------------------------------------------------------------------------");
 }
 
-bool check_packet_end(char data_buf[]){
-  ;
-}
-
-bool check_CRC(char data_buf[]){
+bool check_packet_end(byte data_buff[]) {
   ;
 }
 
-bool check_targetID(char data_buf[]){
+bool check_CRC(byte data_buff[]) {
   ;
 }
-void data_extraction(char data_buf[]){
+
+bool check_targetID(byte data_buff[]) {
   ;
 }
-void buf_flush(char data_buf[]){
+void data_extraction(byte data_buff[]) {
   ;
 }
-int find_header(char rxpacket[]){    
-  int idx;
-  for (idx = 0; idx < (BUF_SIZE - 3); idx++){ 
-    if ((rxpacket[idx] == 0xFF) && (rxpacket[idx+1] == 0xFF) && (rxpacket[idx+2] == 0xFD) && (rxpacket[idx+3] != 0xFD))
+void buff_flush(byte data_buff[]) {
+  ;
+}
+
+int find_header(byte packet[]) {
+  int target_index = virtual_buffer_head_decrement(buff_head);
+  for(int i=3;i>=0;i--){
+    if(!(packet[target_index] == HEADER[i]))return false;
+    target_index = virtual_buffer_head_decrement(target_index);
+    // Serial1.print("Cmp:");
+    // Serial1.print(packet[target_index],HEX);
+    // Serial1.print(" and ");
+    // Serial1.print(HEADER[i],HEX);
+    // Serial1.print(" RES: ");
+    // Serial1.println((packet[target_index] == HEADER[i]));
+    }
     return true;
-  }
-  return false;
 }
-void loop() {
-  //データ格納部分
-  if(Serial.available()){
-    data_buf[buf_counter]=Serial.read();
-    buf_counter++;
-    if(buf_counter>BUF_SIZE-1){
-      buf_counter=0;
+
+void buffer_head_increment(){
+    int new_head = buff_head+1;
+    if (new_head>BUFF_SIZE-1)new_head=0;
+    buff_head = new_head;
+}
+int8_t virtual_buffer_head_increment(int head,int increment){
+    int new_head = head;
+    for(int i=0;i<increment;i++){
+      new_head++;
+      if (new_head>BUFF_SIZE-1)new_head=0;
     }
-    Serial.write(data_buf,BUF_SIZE);
-    // Serial.println();
+    return new_head;
+}
+int8_t virtual_buffer_head_decrement(int head){
+    int new_head = head-1;
+    if (new_head<0)new_head=BUFF_SIZE-1;
+     return new_head;
+}
+String strPad(int num,int zeroCount){
+  String str = String(num);
+  String returnStr = "";
+  if(zeroCount <= str.length()){
+    return str;
   }
-//  ヘッダ検出部分
-    if(find_header(data_buf)){
-      Serial1.println("find header");
-      if(check_targetID(data_buf){
-        // 本格的読み込み
-        // 最後まで読んだらチェックサム照合
-        if(check_packet_end(data_buf)){
-          if(check_CRC){
-            // 問題なければ値代入
-            data_extraction(data_buf);
-            buf_flush(data_buf);
-          }
-        }
-      }
+  for(int i = 0;i < zeroCount - str.length();i++){
+    returnStr += '0';
+  }
+  return returnStr + str;
+}
+void print_buff(uint8_t target_index){
+  for (int i = 0; i < BUFF_SIZE; i++) {
+      // Serial1.print(data_buff[target_index], HEX);
+      // Serial1.print("INDEX:");
+      // Serial1.print(target_index);
+      Serial1.print(data_buff[target_index],HEX);
+      Serial1.print("|");
+      target_index = virtual_buffer_head_increment(target_index,1);
     }
-    for(int i=0;i<buf_counter;i++){
-      byte buf_tmp=data_buf[i];
-      Serial1.print(buf_tmp,HEX);
-    }
-    delay(1);
     Serial1.println();
 }
+
+void loop() {
+  //データ格納部分
+  if (Serial.available()) {
+    Serial1.println("--------------------------------------------");
+    data_buff[buff_head] = Serial.read();
+    buffer_head_increment();
+    // Serial1.print("INDEX:");
+    // Serial1.print(buff_head);
+    // Serial1.print("  DATA:");
+    // Serial1.println(data_buff[buff_head], HEX);
+    print_buff(0);
+
+    if (find_header(data_buff) == true){
+      // Serial1.println("********************************************");
+      data_head = buff_head;
+      check_targetID_flag=1;
+      index_ID = data_head;
+      index_LENGTH_L = virtual_buffer_head_increment(data_head,1);
+      index_LENGTH_H = virtual_buffer_head_increment(data_head,2);
+      index_INST = virtual_buffer_head_increment(data_head,3);
+      Serial1.print("find header INDEX:");
+      Serial1.println(data_head);
+      //データ処理フラグ。ここからカウントしておく。
+      // print_buff(data_head);
+      // Serial1.println("********************************************");
+    }
+
+    if((check_targetID_flag == 1)&&(buff_head == virtual_buffer_head_increment(index_ID,1))){
+      //一個先まで進んでくれないと前の情報を参照する。
+      Serial1.print("data_head:");
+      Serial1.println(data_head);
+      Serial1.print("index id:");
+      Serial1.println(index_ID);
+      Serial1.print("Check ID:");
+      Serial1.println(data_buff[index_ID]);
+      Serial1.print("Check ID:");
+      Serial1.println(data_buff[index_ID]);
+      if(ID==data_buff[index_ID]){
+        Serial1.println("ID MATCH");
+        check_data_length_flag = 1;
+      }
+      else{
+        Serial1.println("ID MISMATCH");
+        check_data_length_flag = 0;
+      }
+      check_targetID_flag = 0;
+      check_data_length_flag = 1;
+    }
+    if((check_data_length_flag == 1)&&(buff_head==virtual_buffer_head_increment(index_LENGTH_H,1))){
+      Serial1.print("Check data length H:");
+      Serial1.println(data_buff[index_LENGTH_H]);
+      Serial1.print("Check data length L:");
+      Serial1.println(data_buff[index_LENGTH_L]);
+      int16_t data_length = (int16_t) (
+          (((int16_t)data_buff[index_LENGTH_H] <<  8) & 0xFF00)
+        | (((int16_t)data_buff[index_LENGTH_L] <<  0) & 0x00FF)
+        );
+      Serial1.print("Data length:");
+      Serial1.println(data_length);
+      check_data_length_flag = 0;
+      index_data_end = virtual_buffer_head_increment(index_LENGTH_H,data_length);
+      wait_receive_complete = 1:
+    }
+  }
+}
+  //   if (check_targetID(data_buff)) {
+  //     if (check_packet_end(data_buff)) {
+  //       if (check_CRC) {
+  //         // 問題なければ値代入
+  //         data_extraction(data_buff);
+  //         buff_flush(data_buff);
+  //       }
+  //     }
+  //   }
+  // delay(1);
